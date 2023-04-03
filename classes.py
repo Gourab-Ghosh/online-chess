1c65ba6e69c025370030f79b99127889c019e036
Gourab-Ghosh <gourab200ghosh@gmail.com>
2023-04-03 18:31:43 +0530

    added move piece in board

--- classes.py
+++ classes.py
@@ -18,6 +18,11 @@ class Driver:
             return self.driver.find_element(by, value)
         return WebDriverWait(self.driver, wait_time).until(EC.presence_of_element_located((by, value)))
 
+    def find_elements(self, by: By, value: str, wait_time: int = 60) -> None: # Incomplete
+        if wait_time == 0:
+            return self.driver.find_elements(by, value)
+        return self.driver.find_elements(by, value)
+
     def wait(self, wait_time: int = 3) -> None:
         time.sleep(wait_time)
 
@@ -28,16 +33,27 @@ class Square(Driver):
     pass
 
 class Board(Driver):
-    pass
+    
+    def __init__(self, driver=None) -> None:
+        super().__init__(driver)
+        self.board = chess.Board()
 
 class ChessDotComBoard(Board):
 
     def __init__(self, driver) -> None:
         super().__init__(driver)
-        self.chess_board = self.driver.find_element(By.TAG_NAME, "chess-board")
+
+    def scan_board(self):
+        self.chess_board = self.find_element(By.TAG_NAME, "chess-board")
+
+    def detect_board_flipped(self):
+        board = self.get_piece_unordered_map()
+        if board[chess.E1] == chess.Piece.from_symbol("K"):
+            return False
+        return True
 
     def get_piece_unordered_map(self):
-        pieces = self.driver.find_elements(By.CLASS_NAME, "piece")
+        pieces = self.find_elements(By.CLASS_NAME, "piece")
         piece_map = {}
         for piece in pieces:
             try:
@@ -73,11 +89,42 @@ class ChessDotComBoard(Board):
         single_cell_width, single_cell_height = width / 8, height / 8
         return (col + 0.5) * single_cell_width + x, (row + 0.5) * single_cell_height + y
 
+    def move_piece(self, from_square: int, to_square: int, promotion: chess.PieceType = None):
+        from_square_coord = self.square_to_coordinate(from_square)
+        to_square_coord = self.square_to_coordinate(to_square)
+        action = ActionChains(self.driver)
+        offset = (j-i for i, j in zip(from_square_coord, to_square_coord))
+        piece = self.find_element(By.CLASS_NAME, f"square-{from_square % 8 + 1}{from_square // 8 + 1}")
+        action.drag_and_drop_by_offset(piece, *offset).perform()
+        if promotion:
+            promotion_piece_css_selector = f".promotion-piece.{'w' if self.board.turn else 'b'}{' pkbrqk'[promotion]}"
+            promotion_piece = self.find_element(By.CSS_SELECTOR, promotion_piece_css_selector)
+            promotion_piece.click()
+
+    def is_game_over(self):
+        return self.board.is_game_over()
+
+    def push(self, move: chess.Move, drag: bool = True):
+        if move == chess.Move.null():
+            raise Exception
+        self.board.san(move)
+        if self.is_game_over():
+            return
+        if drag:
+            if self.is_game_over():
+                return
+            self.move_piece(move.from_square, move.to_square, move.promotion)
+        print(f"Move: {self.board.san(move)}")
+        self.board.push(move)
+        # self.bot.apply_move(move.uci())
+
+class LichessBoard(Board):
+    pass
+
 class Browser(Driver):
 
     def __init__(self) -> None:
         super().__init__()
-        self.board = Board(self.driver)
         self._logged_in = False
         self.set_ids_and_css()
     
@@ -107,6 +154,7 @@ class ChessDotComBrowser(Browser):
         self.goto('https://www.chess.com')
     
     def set_ids_and_css(self) -> None:
+        self.board = ChessDotComBoard(self.driver)
         self.login_page_link_selector = (By.LINK_TEXT, "Log In")
         self.username_input_selector = (By.ID, "username")
         self.password_input_selector = (By.ID, "password")
@@ -125,7 +173,7 @@ class ChessDotComBrowser(Browser):
             play_button.click()
             if not self._logged_in:
                 play_as_guest_button = self.find_element(By.ID, "guest-button")
-                authentication_levels = self.driver.find_elements(By.CLASS_NAME, "authentication-intro-level")
+                authentication_levels = self.find_elements(By.CLASS_NAME, "authentication-intro-level")
                 authentication_level = authentication_levels[-1]
                 authentication_level.click()
                 play_as_guest_button.click()
@@ -139,6 +187,7 @@ class LichessBrowser(Browser):
         self.goto('https://lichess.org')
     
     def set_ids_and_css(self) -> None:
+        self.board = LichessBoard(self.driver)
         self.login_page_link_selector = (By.LINK_TEXT, "SIGN IN")
         self.username_input_selector = (By.ID, "form3-username")
         self.password_input_selector = (By.ID, "form3-password")
