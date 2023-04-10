0948ee30e95eb6b2224b08d4dfd1153b3b2d9a29
Gourab-Ghosh <gourab200ghosh@gmail.com>
2023-04-10 06:40:54 +0530

    M  credentials.py, M  main.py

--- credentials.py
+++ credentials.py
@@ -1,30 +1,35 @@
-import random, string
+import sys, random, string
 
-random.seed(3)
+seed = 0
 
-username_chars = string.ascii_letters + string.digits
-username_length = random.choice(range(5, 11))
-password_length_per_type = random.choice(range(8, 21)) // 4
+def generate_username_and_password(seed = seed):
+    random.seed(seed)
+    username_length = random.choice(range(5, 11))
+    password_length_per_type = random.choice(range(8, 21)) // 4
+    username = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(username_length))
+    password_array = sum(
+        [[random.choice(password_chars) for _ in range(password_length_per_type)]
+        for password_chars in [
+            string.ascii_lowercase,
+            string.ascii_uppercase,
+            string.digits,
+            string.punctuation,
+        ]],
+        [],
+    )
+    random.shuffle(password_array)
+    password = "".join(password_array)
+    return username, password
 
-USERNAME = "".join(random.choice(username_chars) for _ in range(username_length))
+USERNAME, PASSWORD = generate_username_and_password()
 EMAIL = USERNAME + "@email.com"
 
-password_array = sum(
-    [[random.choice(password_chars) for _ in range(password_length_per_type)]
-    for password_chars in [
-        string.ascii_lowercase,
-        string.ascii_uppercase,
-        string.digits,
-        string.punctuation,
-    ]],
-    [],
-)
-
-random.shuffle(password_array)
-
-PASSWORD = "".join(password_array)
-
 if __name__ == "__main__":
-    print("Username:", USERNAME)
-    print("Email:", EMAIL)
-    print("Password:", PASSWORD)
+    if "--list-users" in sys.argv:
+        for i in range(seed + 1):
+            username = generate_username_and_password(i)[0]
+            print(username)
+    else:
+        print("Username:", USERNAME)
+        print("Email:", EMAIL)
+        print("Password:", PASSWORD)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # https://www.chess.com/game/live/74452837201
 # https://www.chess.com/game/live/74634665629
 # https://www.chess.com/game/live/74743923841
+# https://www.chess.com/game/live/74823225147
 
 from credentials import USERNAME, PASSWORD
 from classes import LichessBrowser, ChessDotComBrowser
